00b31bc8dd3f9193ebba4d5a19469dd5571b325f
Lydia Viñuela Viejo <ranasconpeluca@MacBook-Air-de-Lydia.local>
2019-07-16 22:08:44 +0200

    actualizacion

--- snake.js
+++ snake.js
@@ -1,28 +1,21 @@
-alert("Hello! I am an alert box!!");
+//get the data to interacta with canvas
 const cv = document.getElementById("snakeGame");
-
 const context = cv.getContext("2d");
 
-let d;
+//load the sounds
+const deadSound = new Audio("audio/dead.mp3");
+const eatSound = new Audio("audio/eat.mp3");
+const moveSound = new Audio("audio/up.mp3");
+
+let previousDirection;
 let refreshTime=200;
 
+//tamaño del mapa
+const box=32;
+
+//the event lister
 document.addEventListener("keydown",direction);
 
-function direction(event){
-    if(event.keyCode == 37&& d!="RIGHT"){
-        d ="RIGHT"
-    } else if (event.keyCode == 38&&d!="DOWN"){
-        d = "DOWN"
-    } else if (event.keyCode == 39&&d !="LEFT"){
-        d = "LEFT"
-    } else if (event.keyCode == 40&&d !="UP"){
-        d = "UP"
-    }
-}
-
-//Creamos el map
-
-const box=32;
 
 //cargamos las imágenes
 
@@ -32,6 +25,7 @@ ground.src = "img/ground.png";
 const foodImg = new Image();
 foodImg.src = "img/food.png";
 
+//elementos dl juego
 let snake = [];
 
 snake[0]={
@@ -47,9 +41,27 @@ let food = {
 
 let score = 0;
 
-function draw(){
+function direction(event){
+    if(event.keyCode == 37&& previousDirection!="LEFT"){
+        previousDirection ="RIGHT"
+        moveSound.play();
+    } else if (event.keyCode == 38&& previousDirection!="UP"){
+        previousDirection = "DOWN"
+        moveSound.play();
+    } else if (event.keyCode == 39&&previousDirection !="RIGHT"){
+        previousDirection = "LEFT"
+        moveSound.play();
+    } else if (event.keyCode == 40&&previousDirection !="DOWN"){
+        previousDirection = "UP"
+        moveSound.play();
+    }
+}
+
+
+function drawGame(){
 
     context.drawImage(ground,0,0);
+    //Dibujamos la serpiente
     for(let i = 0;i<snake.length;i++){
         context.fillStyle=(i==0)? "green" :"white";
         context.fillRect(snake[i].x,snake[i].y,box,box);
@@ -60,18 +72,24 @@ function draw(){
         
     }
     
-    
+    //guardamos la variable de la cabeza de la serpiente
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
 
+    if (previousDirection == "LEFT") {snakeX += box;}
+    if (previousDirection == "UP") {snakeY += box;}
+    if (previousDirection == "RIGHT") {snakeX -= box;}
+    if (previousDirection == "DOWN") {snakeY -= box;}
 
-    if (d == "LEFT") {snakeX += box;}
-    if (d == "UP") {snakeY += box;}
-    if (d == "RIGHT") {snakeX -= box;}
-    if (d == "DOWN") {snakeY -= box;}
-
+   
+    //Subimos la puntuacion si como comida
     if(snakeX==food.x &&snakeY==food.y){
-        this.refreshTime-=200;
+        //restamos tiempo al refresco para crear dificultad dinamica
+        refreshTime -=5;
+        score++;
+        eatSound.play();
+        clearInterval(game);
+        game = setInterval(drawGame,refreshTime);
         food = {
             x : Math.floor(Math.random() * 17 + 1) * box,
             y: Math.floor(Math.random() * 15 + 3) * box
@@ -79,12 +97,21 @@ function draw(){
     }else{
         snake.pop();
     }
+
+    
     
     let newHead = {
         x : snakeX,
         y : snakeY
     }
+
+    //si se choca con el muro o si misma se muere
+    if(snakeX<box||snakeX>17*box||snakeY<3*box||snakeY>17*box||collision(newHead,snake)){
+        clearInterval(game);
+        deadSound.play();
+    }
     
+    //añadimos y repintamos la cabeza y la comida
     snake.unshift(newHead);
 
     context.drawImage(foodImg,food.x,food.y);
@@ -98,4 +125,15 @@ function draw(){
 
 }
 
-let game = setInterval(draw,refreshTime);
+//comprueba la colision 
+function collision(head,array){
+    for(let i = 0;i<array.length;i++){
+        if(head.x == array[i].x && head.y==array[i].y){
+            return true;
+        }
+    }
+    return false;
+}
+
+//empieza el refresco de canvas
+let game = setInterval(drawGame,refreshTime);
